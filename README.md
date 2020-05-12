79a0b340137dd4d7d3b388a2f7053eb0051bb0ac
hongpao <hongpao@2dfire.com>
2020-05-12 19:43:37 +0800

    add

---
+++ README.md
@@ -0,0 +1,13 @@
+# @umi-material/umi-block-test
+
+umi block build
+
+## Usage
+
+```sh
+umi block https://github.com/umijs/umi-blocks/tree/master/umi-block-test
+```
+
+## LICENSE
+
+MIT
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import styles from './index.css';
+
+export default function() {
+  return (
+    <div className={styles.normal}>
+      <h1>I am a umi block!</h1>
+      <h2>I am hongpao!</h2>
+    </div>
+  );
+}
